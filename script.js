f9b9a3d917e6390f884c00c95675957d010f2972
Radid Ahammed <radidboktier823@gmail.com>
2025-12-17 19:40:53 +0600

    feat(canvas): apply image filters directly onto canvas rendering

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const filters = {
     max: 200,
     unit: '%',
   },
-  exposure: {
-    value: 100,
-    min: 0,
-    max: 200,
-    unit: '%',
-  },
+  // exposure: {
+  //   value: 100,
+  //   min: 0,
+  //   max: 200,
+  //   unit: '%',
+  // },
   saturation: {
     value: 100,
     min: 0,
@@ -64,6 +64,8 @@ const filters = {
 const imageCanvas = document.querySelector('#image-canvas');
 const imgInput = document.querySelector('#image-input');
 const canvasCtx = imageCanvas.getContext('2d');
+let file = null;
+let image = null;
 
 const filtersContainer = document.querySelector('.filters');
 
@@ -79,10 +81,16 @@ function createFilterElement(name, unit = '%', value, min, max) {
   input.id = name;
 
   const p = document.createElement('p');
-  p.innerText = name;
+  p.innerText = name.charAt(0).toUpperCase() + name.slice(1);
 
   div.appendChild(p);
   div.appendChild(input);
+
+  input.addEventListener('input', (event) => {
+    filters[name].value = input.value;
+    // console.log(input.value);
+    applyFilters();
+  });
 
   return div;
 }
@@ -102,14 +110,32 @@ imgInput.addEventListener('change', (event) => {
   const file = event.target.files[0];
 
   const imagePlaceholder = document.querySelector('.placeholder');
+  imageCanvas.style.display = 'block';
   imagePlaceholder.style.display = 'none';
 
   const img = new Image();
   img.src = URL.createObjectURL(file);
 
   img.onload = () => {
+    image = img;
     imageCanvas.width = img.width;
     imageCanvas.height = img.height;
     canvasCtx.drawImage(img, 0, 0);
   };
 });
+
+function applyFilters() {
+  canvasCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+  canvasCtx.filter = `
+    brightness(${filters.brightness.value}${filters.brightness.unit})
+    contrast(${filters.contrast.value}${filters.contrast.unit})
+    saturate(${filters.saturation.value}${filters.saturation.unit})
+    hue-rotate(${filters.hueRotation.value}${filters.hueRotation.unit})
+    blur(${filters.blur.value}${filters.blur.unit})
+    grayscale(${filters.grayscale.value}${filters.grayscale.unit})
+    sepia(${filters.sepia.value}${filters.sepia.unit})
+    opacity(${filters.opacity.value}${filters.opacity.unit})
+    invert(${filters.invert.value}${filters.invert.unit})
+  `;
+  canvasCtx.drawImage(image, 0, 0);
+}
